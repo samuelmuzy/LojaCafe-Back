9bd6b003e5166a53474a68da83187e4de0257425
samuel muzy <samuelmuzyfernandes4@gmail.com>
2024-10-25 10:52:10 -0300

    criação da verificação se o email ja existe

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request,Response } from 'express'
 import cors from 'cors'
 import connection from './connection'
-import { v4 } from 'uuid';
+import { v7 } from 'uuid';
 
 const app = express();
 
@@ -11,8 +11,8 @@ app.use(cors());
 //endpoint clientes
 app.get('/clientes',async (req:Request,res:Response)=>{
     try{
-        const clientes = await connection('clientes');
-        
+        const clientes = await connection('tbcliente');
+
         res.status(200).send(clientes);
 
     }catch(erro){
@@ -23,7 +23,7 @@ app.get('/clientes',async (req:Request,res:Response)=>{
 app.get('/clientes',async (req:Request,res:Response)=>{
     const {nome} = req.query
     try{
-        const clientes = await connection('clientes')
+        const clientes = await connection('tbcliente')
         .where({'nome_cliente':nome})
 
         res.status(200).send(clientes);
@@ -37,25 +37,36 @@ app.get('/clientes',async (req:Request,res:Response)=>{
 app.post('/clientes',async (req:Request,res:Response)=>{
     const {nome,telefone,email} = req.body;
     
-    const id = v4();
+    const id = v7(); // usar v7
     
     try{
         if(!nome || !telefone || !email){
+            res.status(401);
             throw Error("Campo faltando")
         }
-        await connection('clientes')
+
+        //todo: verificar se email/cadastro já existe, se existe retornar tratamento
+        const verificarEmail = await connection('tbcliente')
+        .where({'dfemail_cliente':email})
+
+        if (verificarEmail.length > 0) {
+            res.status(401)
+            throw Error("Email ja cadastrado");
+        }
+        
+        await connection('tbcliente')
         .insert({
-            'id_cliente':id,
-            'nome_cliente':nome,
-            'telefone':telefone,
-            'email':email
+            'dfid_cliente':id,
+            'dfnome_cliente':nome,
+            'dftelefone_cliente':telefone,
+            'dfemail_cliente':email
         })
         
         res.status(201).send('Cliente cadastrado com sucesso!');
 
     }catch(error:any){
-        console.log(error.sqlMessage || error.message)
-        res.status(400).send(error.message)
+        const message = (error.sqlMessage || error.message)
+        res.send(message)
     }
 })
 //deletar cliente
@@ -63,21 +74,18 @@ app.delete('/clientes/:idCliente',async (req:Request,res:Response)=>{
     const {idCliente} = req.params;
     
     try{
-        const usuarioExiste = await connection('clientes')
-        .where({'id_cliente':idCliente})
-        .delete();
-
-        if(usuarioExiste === 0){
-            throw Error("user não encontrado");
+        const usuarioExiste = await connection('tbcliente')
+        .where({'dfid_cliente':idCliente})
+        .delete()
+        if(!usuarioExiste){
+            throw Error("user não encontrado");   
         }
-
+        
         res.status(200).send('Cliente deletado com sucesso!')
 
-
-
     }catch(error:any){
-        console.log(error.sqlMessage || error.message)
-        res.status(400).send(error.message)
+        const message = (error.sqlMessage || error.message)
+        res.send(message)
     }
 })
 
