365e832260669e71396b764d8d2ce0d6dedc4f71
GabrielBianquini <gsilvabianquini@gmail.com>
2024-12-01 21:14:49 -0300

    Aplicando arquitetura referente a bebidas

---
+++ src/controller/BebidasController.ts
@@ -0,0 +1,61 @@
+import { alterarBebidaServise, cadastroBebidas,  deletarBebidaNova, exibirBebidas } from "../services/BebidaServise";
+import express, { Request,Response } from 'express'
+
+export const cadastroBebida = async (req:Request,res:Response) =>{
+    const { nome, descricao, disponivel, preco } = req.body;
+    const file = req.file;
+    const token  = req.headers.authorization!;
+
+    try{
+        const novaBebida = await cadastroBebidas(nome,descricao,preco,disponivel,file?.filename as string,token);
+        res.status(201).send(novaBebida);
+    }catch (error: any) {
+        const statusCode = error.status || 500; // Padrão para 500 se não for definido
+        const message = error.message || "Erro interno do servidor";
+
+        res.status(statusCode).send(error.sqlMessage || message);
+    }
+}
+
+export const buscarTodasBebidas = async (req:Request,res:Response) =>{
+    const {nomeBebida,limit,page} = req.query
+    try{
+        const bebidas = await exibirBebidas(typeof nomeBebida === 'string' ? nomeBebida : '',limit as string,page as string);
+        res.status(200).send(bebidas);
+    }catch (error: any) {
+        const statusCode = error.status || 500; // Padrão para 500 se não for definido
+        const message = error.message || "Erro interno do servidor";
+
+        res.status(statusCode).send(error.sqlMessage || message);
+    }
+}
+
+export const excluirBebida = async (req:Request,res:Response) =>{
+    const {idBebida} = req.params
+    const token  = req.headers.authorization!;
+    try{
+        const bebidaDeletada = await deletarBebidaNova(idBebida,token);
+        res.status(200).send(bebidaDeletada);
+    }catch (error: any) {
+        const statusCode = error.status || 500; // Padrão para 500 se não for definido
+        const message = error.message || "Erro interno do servidor";
+
+        res.status(statusCode).send(error.sqlMessage || message);
+    }
+}
+
+export const alterarBebidaController = async (req:Request,res:Response) =>{
+    const {nome,descricao,preco,disponivel} = req.body;
+    const file = req.file;
+    const token  = req.headers.authorization!;
+    const { idBebida } = req.params;
+    try{
+        const bebidaAlterada = await alterarBebidaServise(idBebida,nome,descricao,preco,disponivel,file?.filename as string,token);
+        res.status(200).send(bebidaAlterada);
+    }catch (error: any) {
+        const statusCode = error.status || 500; // Padrão para 500 se não for definido
+        const message = error.message || "Erro interno do servidor";
+
+        res.status(statusCode).send(error.sqlMessage || message);
+    }
+}
---
+++ src/services/BebidaServise.ts
@@ -0,0 +1,107 @@
+import { v7 } from "uuid";
+import { alterarBebida, buscarBebidas, cadastrarNovaBebida, deletarBebidaPorId, VerificarIdBebida } from '../data/BebidaData'
+import { getTokenData } from "../middlewares/Authenticator";
+
+export const cadastroBebidas = async (nome:string,descricao:string,preco:number,disponivel:boolean,file:string,token:string) =>{
+    const idBebida = v7()
+
+    const tokenData = getTokenData(token);
+
+    if(!tokenData){
+        throw {status: 404 , message: "Token invalido"}
+    }
+
+    try {
+        if (!nome || !descricao || !preco || disponivel === undefined || !file) {
+            throw { status: 400, message: "Campo faltando" };
+        }
+
+        await cadastrarNovaBebida(idBebida, nome, descricao, preco, disponivel, file);
+        return "Bebida cadastrada com sucesso";
+    } catch (error: any) {
+        throw { status: error.status || 500, message: error.message || "Erro ao cadastrar bebida" };
+    }
+};
+
+export const exibirBebidas = async (nomeBebida: string, limit: string, page: string) => {
+    try {
+        const limitValid = limit || "10"; 
+        const offsetValid = page || ""; 
+
+        const bebidas = await buscarBebidas(nomeBebida || '', limitValid, offsetValid);
+
+        if (nomeBebida && bebidas.length === 0) {
+            throw { status: 404, message: "Bebida não encontrada!" };
+        }
+
+        return bebidas;
+    } catch (error: any) {
+        throw { status: error.status || 500, message: error.message || "Erro ao buscar bebidas" };
+    }
+};
+
+export const deletarBebidaNova = async (idBebida: string,token:string) => {
+    try {
+
+        const tokenData = getTokenData(token);
+
+        if(!tokenData){
+            throw {status: 404 , message: "Token invalido"}
+        }
+        if (!idBebida) {
+            throw { status: 400, message: "Campo faltando" };
+        }
+        const idsBebidasValidas = await VerificarIdBebida(idBebida);
+
+        if (idsBebidasValidas.length === 0) {
+            throw { status: 404, message: "Bebida não encontrada!" };
+        }
+
+        await deletarBebidaPorId(idBebida);
+
+        return "Bebida deletada com sucesso";
+    } catch (error: any) {
+        throw { status: error.status || 500, message: error.message || "Erro ao deletar bebida" };
+    }
+};
+
+export const alterarBebidaServise = async (
+    idBebida: string,
+    nome: string,
+    descricao: string,
+    preco: number,
+    disponivel: boolean,
+    file: string,
+    token:string
+) => {
+    try {
+        const tokenData = getTokenData(token);
+
+        if(!tokenData){
+            throw {status: 404 , message: "Token invalido"}
+        }
+
+        if (!nome && !descricao && !preco && !disponivel && !file) {
+            throw { status: 400, message: "Campo faltando" };
+        }
+
+        const idsBebidasValidas = await VerificarIdBebida(idBebida);
+
+        if (idsBebidasValidas.length === 0) {
+            throw { status: 404, message: "Bebida não encontrada!" };
+        }
+
+        const dadosAtualizacao: any = {};
+        if (nome) dadosAtualizacao.dfnome_bebida = nome;
+        if (descricao) dadosAtualizacao.dfdescricao_bebida = descricao;
+        if (preco) dadosAtualizacao.dfpreco = preco;
+        if (disponivel !== undefined) dadosAtualizacao.dfbebida_disponivel = disponivel;
+        if (file) dadosAtualizacao.dfcaminho_imagem = `/file/${file}`;
+
+        await alterarBebida(idBebida, dadosAtualizacao);
+
+        return "Bebida alterada com sucesso";
+    } catch (error: any) {
+        throw { status: error.status || 500, message: error.message || "Erro ao alterar bebida" };
+    }
+};
